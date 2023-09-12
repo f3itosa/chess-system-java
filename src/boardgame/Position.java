943c83e17ccab7818f60f9a08885cba543833618
Eric Feitosa <eric_feitosa13@hotmail.com>
2023-09-12 20:16:43 -0300

    Peças no tabuleiro

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -3,36 +3,36 @@ package boardgame;
 public class Position {
 	
 	private int row;
-	private int colum;
+	private int column;
 	
 	public Position() {
 		
 	}
 
-	public Position(int row, int colum) {
+	public Position(int row, int column) {
 		this.row = row;
-		this.colum = colum;
+		this.column = column;
 	}
 
 	public int getRow() {
 		return row;
 	}
 
-	public int getColum() {
-		return colum;
+	public int getColumn() {
+		return column;
 	}
 
 	public void setRow(int row) {
 		this.row = row;
 	}
 
-	public void setColum(int colum) {
-		this.colum = colum;
+	public void setColumn(int column) {
+		this.column = column;
 	}
 	
 	@Override
 	public String toString() {
-		return row + ", " + colum;
+		return row + ", " + column;
 	}
 
 }
